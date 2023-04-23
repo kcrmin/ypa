eca5455b8711c7717238370466d14f2d89e0e8cb
maggieguo <7maggie8@gmail.com>
2023-04-24 00:16:51 +0800

    mood&todo screen logic implemented

--- lib/screen/todo_screen.dart
+++ lib/screen/todo_screen.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:ypa/data/todo_item.dart';
 import '../component/todoList.dart';
 import 'dart:developer';
-
+import 'package:ypa/database/drift_database.dart';
+import 'package:get_it/get_it.dart';
 import '../data/daily_mood.dart';
+import 'mood_screen.dart';
 
 class ToDoScreen extends StatefulWidget {
   final DateTime selectedDay;
@@ -29,46 +31,81 @@ class _ToDoScreenState extends State<ToDoScreen> {
     focusedDay = widget.focusedDay;
   }
 
-  
-  List<todoItem> todos = [
-    todoItem(DateTime.utc(2023, 4, 16), false, 'first todo'),
-    todoItem(DateTime.utc(2023, 4, 16), false, 'second todo'),
-    todoItem(DateTime.utc(2023, 4, 16), true, 'third todo'),
-    todoItem(DateTime.utc(2023, 4, 16), true, 'fourth'),
-    todoItem(DateTime.utc(2023, 4, 21), false, '5th'),
-    todoItem(DateTime.utc(2023, 4, 21), false, '6th'),
-    todoItem(DateTime.utc(2023, 3, 16), false, '7th'),
-    todoItem(DateTime.utc(2023, 3, 16), false, '8th'),
-  ];
-  List<dailyMood> moodList = [
-    dailyMood (DateTime.utc(2023, 3, 16), '7887de'),
-    dailyMood (DateTime.utc(2023, 3, 9) ,'7887de'),
-    dailyMood (DateTime.utc(2023, 4, 16) ,'7887de'),
-    dailyMood (DateTime.utc(2023, 4, 20) ,'7887de'),
-    dailyMood (DateTime.utc(2023, 4, 16) ,'7887de'),
-    dailyMood (DateTime.utc(2023, 4, 22) ,'7887de'),
-    dailyMood (DateTime.utc(2023, 4, 1) ,'7887de'),
-    
-    ];
+
+
   Widget build(BuildContext context){
-    log('todo screen passed selected day: $selectedDay');
     return Scaffold(
       body: 
-        Center(
-          child: 
-            TodoList(
-              todos: todos,
-              selectedDay: selectedDay,
-              focusedDay:focusedDay,
-              onDaySelected: onDaySelected,
-              changeCompletionStatus: changeCompletionStatus,
-              deleteTodo: deleteTodo,
-              addTodo: addTodo,
-              moodList: moodList,
-          )
+        FutureBuilder<List<Mood>>(
+          future:_getMoodFromDatabase(),
+          builder: (context, snapshot) {
+            List<int> colorIdList = [1,1,1,3,4,5,2,6];
+            List<DateTime> dateList = [
+            DateTime(2023,4,23),
+            DateTime(2023,4,24),
+            DateTime(2023,4,25),
+            DateTime(2023,4,26),
+            DateTime(2023,3,23),
+            DateTime(2023,3,15),
+            DateTime(2023,4,29),
+            DateTime(2023,4,20),];
+            // List<int> colorIdList = [];
+            // List<DateTime> dateList = [];
+            // if(snapshot.hasData){
+            //   colorIdList = snapshot.data!.map((e) => e.colorId).toList();
+            //   dateList = snapshot.data!.map((e) => e.date).toList();
+            // }
+            return Center(
+              child: 
+                FutureBuilder<List<Todo>>(
+                  future: _getTodosFromDatabase(),
+                  builder: (context, snapshot) {
+                    //List<todoItem> todos = [];
+                    //  if(snapshot.hasData){
+                    //     List<DateTime>todoDate = snapshot.data!.map((e) => e.date).toList();
+                    //     List<bool>completedList = snapshot.data!.map((e) => e.completed).toList();
+                    //     List<String>title = snapshot.data!.map((e) => e.title).toList();
+                    //     for(int i=0;i<title.length;i++){
+                    //       todos.add(todoItem(todoDate[i], completedList[i], title[i]));
+                    //     }
+                    //   }
+                    List<todoItem>todos = [
+                      todoItem(DateTime.utc(2023, 4, 16), false, 'first todo'),
+                      todoItem(DateTime.utc(2023, 4, 16), false, 'second todo'),
+                      todoItem(DateTime.utc(2023, 4, 16), true, 'third todo'),
+                      todoItem(DateTime.utc(2023, 4, 16), true, 'fourth'),
+                      todoItem(DateTime.utc(2023, 4, 21), false, '5th'),
+                      todoItem(DateTime.utc(2023, 4, 21), false, '6th'),
+                      todoItem(DateTime.utc(2023, 3, 16), false, '7th'),
+                      todoItem(DateTime.utc(2023, 3, 16), false, '8th'),
+                    ];
+                    return TodoList(
+                      todos: todos,
+                      selectedDay: selectedDay,
+                      focusedDay:focusedDay,
+                      onDaySelected: onDaySelected,
+                      changeCompletionStatus: changeCompletionStatus,
+                      deleteTodo: deleteTodo,
+                      addTodo: addTodo,
+                      moodList: getMoodList(colorIdList, dateList),
+              );
+                  }
+                )
+            );
+          }
         ),
     );
   }
+
+  Future<List<Mood>> _getMoodFromDatabase() async {
+    return await GetIt.I<LocalDatabase>().getMoods();
+  }
+
+  Future<List<Todo>> _getTodosFromDatabase() async {
+    return await GetIt.I<LocalDatabase>().getTodos();
+  }
+
+
   onDaySelected(DateTime selectedDay, DateTime focusedDay) {
     setState(() {
       this.selectedDay = selectedDay;
